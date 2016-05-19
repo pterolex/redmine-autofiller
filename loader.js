c983b662844ef2e1bc5b9b4649c1dff357498c7b
S.Iaroshenko <iaroshenko@gmail.com>
2016-05-19 18:04:12 +0300

    Trying to fix css loading

--- loader.js
+++ loader.js
@@ -7,7 +7,9 @@
     }
 
     var gitHub = "https://raw.githubusercontent.com/s-iaroshenko";
-    $("head").append("<link rel='stylesheet' href='" + gitHub + "/redmine-autofiller/master/autofill.css" + "' type='text/css' />");
+
+    var cssHref = gitHub + "/redmine-autofiller/master/autofill.css";
+    $("<link rel='stylesheet' type='text/css' />").appendTo('head').attr('href', cssHref);
 
     $.getScript("https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.min.js", function() {
         $.getScript(gitHub + "/Multiple-Dates-Picker-for-jQuery-UI/master/jquery-ui.multidatespicker.js", function() {
