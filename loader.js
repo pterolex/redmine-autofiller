7784dabf3848c5ce8f9cba105c75bffe085c27b6
S.Iaroshenko <iaroshenko@gmail.com>
2016-05-19 20:21:57 +0300

    Minified loader

--- loader.js
+++ loader.js
@@ -1,21 +1,15 @@
 ﻿(function($) {
-    if ($("#loader").length === 0) {
-        var loader = $("<div id='loader'>Loading...</div>");
-        $("#content").prepend(loader);
-    } else {
-        $("#loader").show();
-    }
-
-    var gitHub = "https://raw.githubusercontent.com/s-iaroshenko";
-    $.ajax(gitHub + "/redmine-autofiller/master/autofill.css").done(function(data) {
-        $("<style type='text/css'>\n" + data + "</style>").appendTo("head");
-        $.getScript("https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.min.js", function() {
-            $.getScript(gitHub + "/Multiple-Dates-Picker-for-jQuery-UI/master/jquery-ui.multidatespicker.js", function() {
-                $.getScript(gitHub + "/redmine-autofiller/master/setup.js", function() {
-                    $("#loader").hide();
-                    window.setup();
-                });
-            });
-        });
+ $("<div id='ldr'>Loading...</div>").prependTo("#content");
+ var gH="https://raw.githubusercontent.com/s-iaroshenko";
+ $.ajax(gH+"/redmine-autofiller/master/autofill.css").done(function(css){
+  $("<style type='text/css'>"+css+"</style>").appendTo("head");
+  $.getScript("https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.min.js",function(){
+   $.getScript(gH+"/Multiple-Dates-Picker-for-jQuery-UI/master/jquery-ui.multidatespicker.js",function(){
+    $.getScript(gH+"/redmine-autofiller/master/setup.js",function(){
+     $("#ldr").hide();
+     setup();
     });
+   });
+  });
+ });
 }(jQuery));
