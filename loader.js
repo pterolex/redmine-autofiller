530852ff4d885aa910ad98c77dff5faad55bc846
S.Iaroshenko <iaroshenko@gmail.com>
2016-05-23 18:39:30 +0300

    Added test branch to reference

--- loader.js
+++ loader.js
@@ -1,11 +1,11 @@
 ﻿(function($) {
  $("<div id='ldr'>Loading...</div>").prependTo("#content");
- var gH="https://rawgit.com/s-iaroshenko";
- $.ajax(gH+"/redmine-autofiller/master/autofill.css").done(function(css){
+ var gH="https://rawgit.com/s-iaroshenko",br="test";
+ $.ajax(gH+"/redmine-autofiller/"+br+"/autofill.css").done(function(css){
   $("<style type='text/css'>"+css+"</style>").appendTo("head");
   $.getScript("https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.min.js",function(){
-   $.getScript(gH+"/Multiple-Dates-Picker-for-jQuery-UI/master/jquery-ui.multidatespicker.js",function(){
-    $.getScript(gH+"/redmine-autofiller/master/setup.js",function(){
+   $.getScript(gH+"/Multiple-Dates-Picker-for-jQuery-UI/"+br+"/jquery-ui.multidatespicker.js",function(){
+    $.getScript(gH+"/redmine-autofiller/"+br+"/setup.js",function(){
      var d=new Date(),c="ukr",h;
      $.getJSON("http://kayaposoft.com/enrico/json/v1.0/?action=getPublicHolidaysForMonth"+
      "&month="+d.getMonth()+"&year="+d.getFullYear()+"&country="+c).done(function(r){h=r;}).always(function(){
